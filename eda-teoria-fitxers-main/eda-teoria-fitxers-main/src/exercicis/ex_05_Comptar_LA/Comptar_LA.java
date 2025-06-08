0b24c655e03770c925da96f178e24cffe247001f
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-06-08 20:59:41 +0200

    Update Comptar_LA.java

--- eda-teoria-fitxers-main/eda-teoria-fitxers-main/src/exercicis/ex_05_Comptar_LA/Comptar_LA.java
+++ eda-teoria-fitxers-main/eda-teoria-fitxers-main/src/exercicis/ex_05_Comptar_LA/Comptar_LA.java
@@ -24,6 +24,27 @@ public class Comptar_LA {
 
 
         /* COMPLETAR */
+        try{
+            entrada = new BufferedReader(new fileReader (file));
+        }
+        catch (IOException e){
+            System.out.print ("Hi ha hagut algun problema amb el file");
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        try {
+            linia = entrada.readLine()
+                while (linia!= null){
+                aparicions += processarLinia(linia);
+                linia = entrada.readLine();
+                }
+        }
+        catch (IOException e){
+            System.out.print("a")
+                e.printStackTrace();
+            System.exit(0);
+        }
 
         System.out.println("la cadena LA apareix "+aparicions+" vegades");
 
@@ -43,6 +64,11 @@ public class Comptar_LA {
         /* COMPLETAR */
         // us pot ser útil una iteració en què es fa ús del mètode
         // indexOf en la seva versió de dos paràmetres...
+
+        while (pos != -1){
+            resultat +=1;
+                pos = linia.indexOf("LA", pos+1);
+        }
 
         return resultat;
     }
